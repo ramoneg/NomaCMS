220fe749126e578a47340c6d0d43fd54a0ec26bf
Ramon Egger <egger@racerfish.com>
2021-01-15 21:12:36 +0100

    Removed axios default config

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,6 @@ export default {
         },
     },
     axios: {
-        // baseURL: `https://cdn.contentful.com/spaces/${config.CTF_SPACE_ID}/environments/${config.CTF_ENVIRONMENT}/`,
+        //
     },
 };
