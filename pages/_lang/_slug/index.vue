83543b0dfd12b22cd16c9e130f51d19df58721bf
Ramon Egger <egger@racerfish.com>
2021-01-17 22:26:45 +0100

    Set dynamic seo data and page title

--- pages/_lang/_slug/index.vue
+++ pages/_lang/_slug/index.vue
@@ -1,23 +1,36 @@
 <template>
     <div>
-        <div v-if="page && page.length > 0">
-            {{page}}
-        </div>
-        <div v-else>
-            Page not found!
-        </div>
+        <div v-if="!page">Page not found!</div>
+        <page-renderer :page="page" v-else />
     </div>
 </template>
 
 <script>
+import PageRenderer from '~/components/NomaCMS/PageRenderer.vue'
 export default {
+    components: {
+        PageRenderer,
+    },
+    head() {
+        return {
+            title: this.page.fields.title,
+            meta: [
+                {
+                    name: this.page.fields.seoTitle,
+                    content: this.page.fields.seoMeta,
+                },
+            ],
+        }
+    },
     async asyncData({ store, env, route }) {
         const config = { env: env, route: route }
         await store.dispatch('pages/fetchPages', config)
 
-        const page = store.state.pages.pages.filter(page => {
-            return page.fields.slug.toLowerCase() == route.params.slug.toLowerCase()
-        })
+        const page = store.state.pages.pages.filter((page) => {
+            return (
+                page.fields.slug.toLowerCase() ==route.params.slug.toLowerCase()
+            )
+        })[0]
 
         return { page }
     },
