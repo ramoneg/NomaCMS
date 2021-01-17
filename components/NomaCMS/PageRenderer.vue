3a5aa6c39864c6326f3e6176dddf017bccb0058f
Ramon Egger <egger@racerfish.com>
2021-01-17 22:26:59 +0100

    Page content render component

---
+++ components/NomaCMS/PageRenderer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <component v-for="(block) in page.fields.blocks" :key="block.sys.id" :is="block.sys.contentType.sys.id" />
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['page'],
+    name: 'PageRenderer',
+    components: {
+      Hero: () => import('~/components/NomaCMS/Elements/Hero'),
+      TextLeft: () => import('~/components/NomaCMS/Elements/TextLeft'),
+    }
+}
+</script>
